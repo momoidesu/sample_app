cb00f13597f1d1cf0afafc35e6afce548a1b5889
EC2 Default User <ec2-user@ip-172-31-35-140.ap-northeast-1.compute.internal>
2023-12-12 06:11:10 +0000

    [finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,15 +5,29 @@ class ListsController < ApplicationController
 
   def create
     list = List.new(list_params)
-    
+
     list.save
-    
+
     redirect_to '/top'
   end
-  
+
+  def index
+    @lists = List.all
+  end
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title, :body)
   end
 end
+
+On branch main
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   app/controllers/lists_controller.rb
+        modified:   app/views/lists/index.html.erb
+        modified:   config/routes.rb
+
+no changes added to commit (use "git add" and/or "git commit -a")
